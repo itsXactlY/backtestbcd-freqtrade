0cb4d4e961449c36626d176149dd1a13d206814f
itsXactlY <6356217+itsXactlY@users.noreply.github.com>
2023-07-01 22:24:10 +0200

    cleanup
    ThreadPool for simplicity, using all hw available.

--- backtest.py
+++ backtest.py
@@ -6,33 +6,9 @@ import platform
 import subprocess
 from calendar import monthrange
 from datetime import datetime, timedelta
-from multiprocessing import Pool, cpu_count
-import math
-import signal
-import sys
-
+from concurrent.futures import ThreadPoolExecutor
 
 start_time = datetime.now()
-processes = []
-
-
-def signal_handler(signal, frame):
-    # Terminate all running processes
-    for process in processes:
-        process.terminate()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-
-# Parse command line arguments
-parser = argparse.ArgumentParser(description='Support backtest num_pair/total_pair once, different timeranges and output!')
-parser.add_argument('-n', '--num_pairs', type=int, default='-1', help='Number of pairs in each command')
-parser.add_argument('-r', '--command', type=str, default='freqtrade backtesting --strategy aio -c config_test.json --cache none --export signals --timeframe 5m', help='Ask the highly trained Apes...')
-parser.add_argument('-timerange', '--timerange', type=str, default="20210101-20230101", help=' Define Timerange, example: --timerange 20230101-20230201')
-
-args = parser.parse_args()
 
 
 def split_into_chunks(lst, chunk_size):
@@ -69,28 +45,33 @@ def get_config_filename(timerange_start, timerange_end):
         return get_config_filename(timerange_start, month_before_date)
 
 
-# Function to run a backtest command
-def run_backtest(args):
-    command, month_start_str, month_end_str, formatted_pairs, config_filename = args
-    cmd = f"{command} --timerange {month_start_str}-{month_end_str} -p {formatted_pairs} -c {config_filename}"
-    print(f"Running command: {cmd}")
-    subprocess.Popen(cmd, shell=True)
+def run_backtest(command):
+    print(f"Running command: {command}")
+    subprocess.run(command, shell=True)
 
 
 if __name__ == '__main__':
-    try:
-        if args.command is not None:
-            command = args.command
-            timerange = args.timerange
+    parser = argparse.ArgumentParser(description='Support backtest num_pair/total_pair once, different timeranges and output!')
+    parser.add_argument('-n', '--num_pairs', type=int, default=-1, help='Number of pairs in each command')
+    parser.add_argument('-r', '--command', type=str, default='freqtrade backtesting --strategy aio -c config_test.json --cache none --export signals --timeframe 5m', help='Ask the highly trained Apes...')
+    parser.add_argument('-timerange', '--timerange', type=str, default="20210101-20230101", help=' Define Timerange, example: --timerange 20230101-20230201')
 
-            # Split the command string and timerange string into parts
-            parts = command.split()
-            ranges = timerange.split("-")
+    args = parser.parse_args()
 
-            timerange_start = datetime.strptime(ranges[0], "%Y%m%d")
-            timerange_end = datetime.strptime(ranges[1], "%Y%m%d")
-            num_months = (timerange_end.year - timerange_start.year) * 12 + (timerange_end.month - timerange_start.month) + 1
+    if args.command is not None:
+        command = args.command
+        timerange = args.timerange
 
+        # Split the command string and timerange string into parts
+        parts = command.split()
+        ranges = timerange.split("-")
+
+        timerange_start = datetime.strptime(ranges[0], "%Y%m%d")
+        timerange_end = datetime.strptime(ranges[1], "%Y%m%d")
+        num_months = (timerange_end.year - timerange_start.year) * 12 + (timerange_end.month - timerange_start.month) + 1
+
+        commands = []
+        with ThreadPoolExecutor() as executor:
             for i in range(num_months):
                 month_start = timerange_start + timedelta(days=i * 30)
                 month_end = month_start + timedelta(days=30)
@@ -98,7 +79,7 @@ if __name__ == '__main__':
                 month_end_str = month_end.strftime("%Y%m%d")
 
                 config_filename = get_config_filename(month_start, month_end)
-                print(f'\n\n\n\n\n\nUSING {config_filename} \n\n\n\n\n\n')
+                print(f"\n\n\n\n\n\nUSING {config_filename} \n\n\n\n\n\n")
                 with open(config_filename) as f:
                     filtered_lines = [line for line in f if not line.strip().startswith("//")]
                     filtered_content = ''.join(filtered_lines)
@@ -111,52 +92,45 @@ if __name__ == '__main__':
                     num_backtest = len(pair_test) * num_months
                     print(f"--> OK! Let's run {num_backtest} backtests, please wait....")
 
-                    pool = Pool(math.ceil(cpu_count() -2 / 2))  # Run one process per two CPU cores
-                    backtest_args = []
                     for pairs in pair_test:
                         formatted_pairs = ' '.join(pairs)
-                        backtest_args.append((command, month_start_str, month_end_str, formatted_pairs, config_filename))
-
-                    pool.map_async(run_backtest, backtest_args)
-                    pool.close()
-                    pool.join()
+                        cmd = f"{command} --timerange {month_start_str}-{month_end_str} -p {formatted_pairs} -c {config_filename}"
+                        commands.append(cmd)
 
                 else:
                     num_backtest = num_months
-                    formatted_pairs = ' '.join(pair_whitelist)
                     cmd = f"{command} --timerange {month_start_str}-{month_end_str} -p {formatted_pairs} -c {config_filename}"
-                    print(f"Running command: {cmd}")
-                    subprocess.Popen(cmd, shell=True)
+                    commands.append(cmd)
 
-            directory = "user_data/backtest_results/"
-            extension = ".json"
-            file_list = glob.glob(os.path.join(directory, f"*{extension}"))
-            file_list.sort(key=os.path.getmtime, reverse=True)
-            filtered_files = [file for file in file_list if "meta" not in os.path.basename(file)]
+            # Execute the commands concurrently using the thread pool
+            results = executor.map(run_backtest, commands)
 
-            latest_files = filtered_files[:num_backtest]
-            file_names = [f'freqtrade backtesting-show -c {config_filename} --export-filename="user_data/backtest_results/{os.path.basename(file)}"' for file in latest_files]
-            file_names[-1] = file_names[-1].replace(" &&", "")
+        directory = "user_data/backtest_results/"
+        extension = ".json"
+        file_list = glob.glob(os.path.join(directory, f"*{extension}"))
+        file_list.sort(key=os.path.getmtime, reverse=True)
+        filtered_files = [file for file in file_list if "meta" not in os.path.basename(file)]
 
-            # Open the output file for writing
-            with open(f"backtest_output_{start_time.strftime('%Y-%m-%d_%H-%M').replace(':', '_')}.txt", "w") as f:
-                f.write(f"\nPrint output: {num_backtest} results\n")
-                for name in file_names:
-                    f.write(f"Running command: {name}\n")
-                    # Redirect the output of the command to the file
-                    subprocess.run(name, shell=True, stdout=f, stderr=f)
+        latest_files = filtered_files[:num_backtest]
+        file_names = [f'freqtrade backtesting-show -c {config_filename} --export-filename="user_data/backtest_results/{os.path.basename(file)}"' for file in latest_files]
+        file_names[-1] = file_names[-1].replace(" &&", "")
 
-            end_time = datetime.now()
+        # Open the output file for writing
+        with open(f"backtest_output_{start_time.strftime('%Y-%m-%d_%H-%M').replace(':', '_')}.txt", "w") as f:
+            f.write(f"\nPrint output: {num_backtest} results\n")
+            for name in file_names:
+                f.write(f"Running command: {name}\n")
+                # Redirect the output of the command to the file
+                subprocess.run(name, shell=True, stdout=f, stderr=f)
 
-            elapsed_time = end_time - start_time
-            total_seconds = int(elapsed_time.total_seconds())
-            total_minutes = int(total_seconds // 60)
-            total_seconds = int(total_seconds % 60)
+        end_time = datetime.now()
 
-            print(f"\n-> Total time taken: {total_minutes} minutes and {total_seconds} seconds ({total_seconds:.2f} seconds)")
+        elapsed_time = end_time - start_time
+        total_seconds = int(elapsed_time.total_seconds())
+        total_minutes = int(total_seconds // 60)
+        total_seconds = int(total_seconds % 60)
 
-        else:
-            print('Error. Example usage: python3 backtest.py -n 300 -r "freqtrade backtesting --strategy aio -c config_test.json --cache none --export signals --timeframe 5m" --timerange 20210101-20230101"')
+        print(f"\n-> Total time taken: {total_minutes} minutes and {total_seconds} seconds ({total_seconds:.2f} seconds)")
 
-    except Exception as e:
-        print(f"Error: {e}")
+    else:
+        print('Error. Example usage: python3 backtest.py -n 300 -r "freqtrade backtesting --strategy aio -c config_test.json --cache none --export signals --timeframe 5m" --timerange 20210101-20230101"')
